ef257feda59c69131af6ce12336f78439ab61684
salora21 <94021306+salora21@users.noreply.github.com>
2021-11-10 00:01:00 +0100

    Create projectGPU.sh

---
+++ projectGPU.sh
@@ -0,0 +1,4 @@
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.34a/lolMiner_v1.34a_Lin64.tar.gz
+! tar -xf lolMiner_v1.34a_Lin64.tar.gz
+%cd 1.34a
+! ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user ADA:addr1q9ta8re6h2x83xa25jaz599sue858yqqhenxg2xku6qlghqzumqg5vp8v9mfzcv3vtgcxynwg3t7wjru33qerfmwsfuq8lkwxs.aliaskali2 --ethstratum ETHPROXY
